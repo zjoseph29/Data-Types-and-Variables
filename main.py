08cef0fd6c2c9a6c9d563934b809b012b6048eb6
Jose Alejandro Enriquez <alenrqz21@gmail.com>
2020-09-29 23:34:42 -0400

    add data types exercises

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-""" Python for Visual Effects.
+"""Python for Visual Effects.
+
 Assignment #1
 
 Fill the following functions in order to complete the exercise.
 """
+
+# 1.- Make a program that solves and shows the summation of 64 + 32
+
+# 2.- Do the same as the question one but this time use variables instead of 
+# numbers.
+
+# 3.- Make a program that prints a sentence that includes at least 3 variables.
+
+# 4.- Make a program that prints the number of characters in a string. Use a 
+# variable for the string.
+
+# 5.- Given the resolution 1920 x 1080, make a program that prints a string with 
+# the 10% over-scan value of those numbers. The printed string must be as 
+# follows: "The 10% overscan of 1920 is <result width>, and the 1080 is <result height>"
